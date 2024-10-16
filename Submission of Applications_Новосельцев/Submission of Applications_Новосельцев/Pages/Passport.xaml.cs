dfc1c57adce2310d95c7a53346b470589e8e4e78
Nov4ik13 <158300318+Nov4ik13@users.noreply.github.com>
2024-10-16 09:01:27 +0300

    ПР10

---
+++ Submission of Applications_Новосельцев/Submission of Applications_Новосельцев/Pages/Passport.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Submission_of_Applications_Новосельцев.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Passport.xaml
+    /// </summary>
+    public partial class Passport : Page
+    {
+        public Passport()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contacts stat = new Contacts();
+            this.NavigationService.Navigate(stat);
+        }
+    }
+}
---
+++ Submission of Applications_Новосельцев/Submission of Applications_Новосельцев/Pages/Speciality.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Submission_of_Applications_Новосельцев.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Speciality.xaml
+    /// </summary>
+    public partial class Speciality : Page
+    {
+        public Speciality()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Passport stat = new Passport();
+            this.NavigationService.Navigate(stat);
+        }
+    }
+}
